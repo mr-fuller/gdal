8ae746bc267497725d519fa9567e64ccb74d14c1
Mike Fuller <mrfuller460@gmail.com>
2018-06-21 16:27:17 -0400

    remove password

--- tiger_gdb_postgres.bat
+++ tiger_gdb_postgres.bat
@@ -1,20 +1,20 @@
 
 :: Ohio
-:: ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=title6 password=laskdjfhiweiofoies" "C:/Users/fullerm/Documents/ACS Data/TIGER/GDB/OH/tlgdb_2016_a_39_oh.gdb" "Block_Group" -nln tiger_bg_oh_geom_2016
+:: ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=title6" "C:/Users/fullerm/Documents/ACS Data/TIGER/GDB/OH/tlgdb_2016_a_39_oh.gdb" "Block_Group" -nln tiger_bg_oh_geom_2016
 :: ECHO 2016 Block Groups loaded successfully
-::ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=title6 password=laskdjfhiweiofoies" "C:/Users/fullerm/Documents/ACS Data/TIGER/GDB/OH/tlgdb_2017_a_39_oh.gdb" "Block_Group" -nln tiger_bg_oh_geom_2017
+::ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=title6" "C:/Users/fullerm/Documents/ACS Data/TIGER/GDB/OH/tlgdb_2017_a_39_oh.gdb" "Block_Group" -nln tiger_bg_oh_geom_2017
 ::ECHO 2017 Block Groups loaded successfully!
-::ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=title6 password=laskdjfhiweiofoies" "C:/Users/fullerm/Documents/ACS Data/TIGER/GDB/OH/tlgdb_2016_a_39_oh.gdb" "Census_Tract" -nln tiger_ct_oh_geom_2016
+::ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=title6" "C:/Users/fullerm/Documents/ACS Data/TIGER/GDB/OH/tlgdb_2016_a_39_oh.gdb" "Census_Tract" -nln tiger_ct_oh_geom_2016
 ::ECHO 2016 Census Tracts loaded successfully!
-::ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=title6 password=laskdjfhiweiofoies" "C:/Users/fullerm/Documents/ACS Data/TIGER/GDB/OH/tlgdb_2017_a_39_oh.gdb" "Census_Tract" -nln tiger_ct_oh_geom_2017
+::ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=title6" "C:/Users/fullerm/Documents/ACS Data/TIGER/GDB/OH/tlgdb_2017_a_39_oh.gdb" "Census_Tract" -nln tiger_ct_oh_geom_2017
 ::ECHO 2017 Census Tracts loaded successfully!
 
 :: Michigan
-ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=title6 password=laskdjfhiweiofoies" "C:/Users/fullerm/Documents/ACS Data/TIGER/GDB/MI/tlgdb_2016_a_26_mi.gdb" "Block_Group" -nln tiger_bg_mi_geom_2016
+ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=title6" "C:/Users/fullerm/Documents/ACS Data/TIGER/GDB/MI/tlgdb_2016_a_26_mi.gdb" "Block_Group" -nln tiger_bg_mi_geom_2016
 ECHO MI 2016 Block Groups loaded successfully
-ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=title6 password=laskdjfhiweiofoies" "C:/Users/fullerm/Documents/ACS Data/TIGER/GDB/MI/tlgdb_2017_a_26_mi.gdb" "Block_Group" -nln tiger_bg_mi_geom_2017
+ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=title6" "C:/Users/fullerm/Documents/ACS Data/TIGER/GDB/MI/tlgdb_2017_a_26_mi.gdb" "Block_Group" -nln tiger_bg_mi_geom_2017
 ECHO 2017 MI Block Groups loaded successfully!
-ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=title6 password=laskdjfhiweiofoies" "C:/Users/fullerm/Documents/ACS Data/TIGER/GDB/MI/tlgdb_2016_a_26_mi.gdb" "Census_Tract" -nln tiger_ct_mi_geom_2016
+ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=title6" "C:/Users/fullerm/Documents/ACS Data/TIGER/GDB/MI/tlgdb_2016_a_26_mi.gdb" "Census_Tract" -nln tiger_ct_mi_geom_2016
 ECHO 2016 MI Census Tracts loaded successfully!
-ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=title6 password=laskdjfhiweiofoies" "C:/Users/fullerm/Documents/ACS Data/TIGER/GDB/MI/tlgdb_2017_a_26_mi.gdb" "Census_Tract" -nln tiger_ct_mi_geom_2017
+ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=title6" "C:/Users/fullerm/Documents/ACS Data/TIGER/GDB/MI/tlgdb_2017_a_26_mi.gdb" "Census_Tract" -nln tiger_ct_mi_geom_2017
 ECHO 2017 MI Census Tracts loaded successfully!
